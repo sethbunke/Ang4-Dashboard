3b67248dd11ac0ec9e41b84c44d9af23f234ff4b
sethbunke <sjb_strat@hotmail.com>
2017-09-22 23:45:24 +0000

    add gist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GistComponent } from './gist/gist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GistComponent
   ],
   imports: [
     BrowserModule
